e2675bed4ebfc557915fd75fa2074f041cca822d
mayu meguro <mayu.meguro@ysinc.co.jp>
2025-08-13 15:06:52 +0900

    リセット

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,35 @@
-import  FamilyName  from "./components/FamilyName";
-import  GivenName  from "./components/GivenName";
-import  ChildComponent  from "./components/ChildComponent";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-function App(){
-  return(
-    <div>
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
       <div>
-        <FamilyName name="山田" />/<GivenName name="一郎"/>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-      <div>
-        <FamilyName name="鈴木" />/<GivenName name="二郎"/>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
       </div>
-      <div>
-        <FamilyName name="佐藤" />/<GivenName name="一郎"/>
-      </div>
-
-<ChildComponent>
-childrenを使うと、任意の子要素を受け取ることができます！！
-</ChildComponent>
-    </div>
-
-
-  );
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
-export default App;
 
+export default App
